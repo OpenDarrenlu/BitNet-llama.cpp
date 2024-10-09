df9a3bd7ee4f8baf628d0660d0969c1939d7de67
potassiummmm <zhou.hansong@outlook.com>
2024-10-10 00:33:23 +0800

    fix gitignore

---
+++ 3rdparty/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 3659;
+char const *LLAMA_COMMIT = "79b5ed74";
+char const *LLAMA_COMPILER = "Apple clang version 15.0.0 (clang-1500.0.40.1)";
+char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin23.6.0";
